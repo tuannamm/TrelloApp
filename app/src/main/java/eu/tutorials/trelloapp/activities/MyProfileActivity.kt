356cc0e449bffd2cb5aa608b4eb7b6bc43be68f0
Tuan Nam <nttnnam@gmail.com>
2023-04-21 11:21:38 +0700

    refactor

--- app/src/main/java/eu/tutorials/trelloapp/activities/MyProfileActivity.kt
+++ app/src/main/java/eu/tutorials/trelloapp/activities/MyProfileActivity.kt
@@ -7,7 +7,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
